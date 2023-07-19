449512b4212684144a07f28a53d5918e6ff5c115
zlnew <maulana4pz@gmail.com>
2023-07-19 18:28:09 +0700

    Convert image to webp

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -11,7 +11,7 @@ const fullYear: number = new Date().getFullYear();
     <aside class="max-w-62.5 z-990 fixed inset-y-0 my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between overflow-y-hidden rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
         <div class="h-19.5">
             <a class="block px-4 py-4 m-0 text-sm whitespace-nowrap text-dark" href="#">
-                <img src="/storage/logo.png" class="inline transition-all duration-200 border border-primary" alt="main_logo" />
+                <img src="/storage/logo/logo.webp" class="inline transition-all duration-200 border border-primary" alt="main_logo" />
                 <span class="ml-1 font-semibold transition-all duration-200"></span>
             </a>
         </div>
